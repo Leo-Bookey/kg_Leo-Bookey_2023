f1f8b68687a8b0a4d27594ae615523c766f24894
Leo <leobookey@berkeley.edu>
2021-04-26 14:50:45 -0700

    Added all necessary functions, beginning implementation

--- main.js
+++ main.js
@@ -1,2 +1,21 @@
+/**
+* The following program take command line arguments for
+* an array of integers, then printing the phonetic equivalent
+* for each integer in the array
+* @author Leo Bookey
+*/
+
+
 var myArgs = process.argv.slice(2);
 console.log('myArgs: ', myArgs);
+
+
+/**
+* Function that converts one integer
+* @param  {integer} rawInt  integer to be converted
+* @return {String}          Phonetic equivalent
+*/
+function integerToWord(rawInt) {
+
+
+}
